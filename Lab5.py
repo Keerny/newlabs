794e2bffdb64df0a6ebf341d0113435373129b6d
Keerny <56045941+Keerny@users.noreply.github.com>
2021-04-28 19:13:27 +0300

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,11 +1,13 @@
 import random
 import math
-print ("points") #слово после которого нужно вводить кол-во точек
-a = int(input ()) #кол-во точек
+a = 1.0
 k = 0.0
-for i in range(a):
+f = 1
+while f > 0.0001:
     x = random.random()
     y = random.random()
     k += (x * x + y * y < 1.0) #расчёт сколько точек попали в окружность
-print (4 * k / a) #умножение на 4(полный круг) и деление на общее кол-во точек
+    f = (math.fabs(math.pi - (4 * k / a)))
+    print (4 * k / a) #умножение на 4(полный круг) и деление на общее кол-во точек
+    a += 1
 print (math.pi) #ввод pi из библиотеки питона
