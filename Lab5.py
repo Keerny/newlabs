2f65cb32c39c08402734183947271e859f3e45e3
Keerny <56045941+Keerny@users.noreply.github.com>
2021-05-15 23:30:28 +0300

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,8 +1,13 @@
 import random
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+
 a = 1.0
 k = 0.0
 f = 1
+i = []
+
 while f > 0.0001: #проверка на разницу
     x = random.random()
     y = random.random()
@@ -10,4 +15,14 @@ while f > 0.0001: #проверка на разницу
     f = (math.fabs(math.pi - (4 * k / a))) #считаем разницу
     print (4 * k / a) #умножение на 4(полный круг) и деление на общее кол-во точек
     a += 1 #прибавляем к точке
+    i.append(math.fabs(math.pi - (4 * k / a))
+                              
+                              
 print (math.pi) #ввод pi из библиотеки питона
+                              
+                              
+ax.plot(i, label='Pi')
+ax.legend()
+
+
+plt.show()
