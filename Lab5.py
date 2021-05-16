00d5cf8f30fb27c67209a95f15e25ecc629c2de8
Keerny <56045941+Keerny@users.noreply.github.com>
2021-05-16 13:14:24 +0300

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -7,17 +7,15 @@ a = 1.0
 k = 0.0
 f = 1
 i = []
-l = 0
 
 while f > 0.0001: #проверка на разницу
-    l +=l
     x = random.random()
     y = random.random()
     k += (x * x + y * y < 1.0) #расчёт сколько точек попали в окружность
     f = (math.fabs(math.pi - (4 * k / a))) #считаем разницу
     print (4 * k / a) #умножение на 4(полный круг) и деление на общее кол-во точек
     a += 1 #прибавляем к точке
-    i.append(math.fabs(math.pi - (4 * k / a), l))
+    i.append(math.fabs(math.pi - (4 * k / a)))
                               
                               
 print (math.pi) #ввод pi из библиотеки питона
